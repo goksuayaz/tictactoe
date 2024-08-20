a31eaf929db4d9b8b9acca906a23b9e0c1a2dd99
Göksu Ayaz <goksuayaz79@gmail.com>
2024-08-20 17:03:18 +0300

    TicTacToe

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TicTacToe from './components/tic-tac-toe/index.jsx'; // Doğru import yolu
+
+export default function App() {
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  );
+}
